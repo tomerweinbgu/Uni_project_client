c6ef4406f94a953d1da66259c2e2f7b397443a5a
tomerweinbgu <weintome@post.bgu.ac.il>
2024-04-17 20:21:31 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StaffQuestionGeneration from "./components/StaffQuestionGeneration/StaffQuestionGeneration";
+import Lobby from "./components/Lobby/Lobby";
+import Student from "./components/Student/Student";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Lobby />} />
+          <Route path="/staff" element={<StaffQuestionGeneration />} />
+          <Route path="/student" element={<Student />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
